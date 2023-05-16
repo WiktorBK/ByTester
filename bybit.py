38bf0ba0bb04a5b4d428eccd771ef47caadcd077
wiktor bozek <wiktorsebbozek@gmail.com>
2023-05-16 21:58:41 +0200

    Bybit: query data

--- bybit.py
+++ bybit.py
@@ -13,7 +13,19 @@ class Bybit:
         try: return HTTP("https://api.bybit.com", request_timeout= 60)
         except: print(f"Couldn't open session:\n {traceback.format_exc()}")
 
- 
-        
+    def get_format_data(self, symbol):
+            data = self.get_data(MONTHS, symbol)
+            if data==[]: print("Data period is too small")
+            try:
+                df = pd.DataFrame(data)
+                df = df.iloc[:, 5:11]
+                df.columns = ['Time', 'Open', 'High', 'Low', 'Close' , 'Volume']
+                df = df.set_index('Time')
+                df.index = pd.to_datetime(df.index, unit='s')
+                df = df.astype(float)
+                return df
+            except: print(f"Couldn't format data:\n {traceback.format_exc()}")
+
+
 
 
