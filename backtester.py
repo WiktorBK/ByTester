dfff8847868b07ad8737e8f50ca2904e118c5f66
wiktor bozek <wiktorsebbozek@gmail.com>
2023-05-16 21:33:21 +0200

    Backtest: find trade opportunities for given period

--- backtester.py
+++ backtester.py
@@ -1,7 +1,6 @@
 from config import *
 from strategy import Strategy
 
-
 class Backtest:
 
     def calculate_takeprofit(close_price, side):
@@ -12,8 +11,29 @@ class Backtest:
         multiplier = -1 if side=="BUY" else 1
         return float(close_price * (1 + multiplier * SL_PERC)) # multiply by -1 if side == "BUY"
     
+    @classmethod
+    def get_trades(cls, df):
+        trades=[]
+        ema_diff = Strategy.ema_difference(df)
+        #iterate through the entire period of given time and searching for crosses
+        for i in range(198, len(ema_diff) - 1):
+            # find longs
+            if ema_diff[i] < 0 and ema_diff[i+1] >= 0:
+                # calculating prices
+                tp = cls.calculate_takeprofit(df['Close'][i+1], "BUY")
+                sl = cls.calculate_stoploss(df['Close'][i+1], "BUY")
+                trades.append({'index':i+1, 'side': "BUY", 'time':df.index[i+1], 'open': df['Open'][i+1], 'tp': tp, "sl": sl})
+
+            # find shorts
+            elif ema_diff[i] > 0 and ema_diff[i+1] <= 0:
+                tp = cls.calculate_takeprofit(df['Close'][i+1], "SELL")
+                sl = cls.calculate_stoploss(df['Close'][i+1], "SELL")
+                trades.append({'index':i+1,'side': "SELL", 'time':df.index[i+1], 'open': df['Open'][i+1], 'tp': tp, "sl": sl})
+
+        return trades
 
 
+   
 
 
 
