6f191a8017fcbd617e5a48cbe4db80a81d7f2107
wiktor bozek <wiktorsebbozek@gmail.com>
2023-05-25 10:31:28 +0200

    Print info before each backtest

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from backtester import *
 
 bb = Bybit()
 if __name__ == "__main__":
+    print(f"Testing {len(SYMBOLS)} pairs\nCapital per one pair: {CAPITAL_USD} USD\nPeriod: {MONTHS} month/s\nInterval:{INTERVAL}min\nEMAS:{EMA1}/{EMA2}")
     for symbol in SYMBOLS:
 
         # run backtest for each symbol
